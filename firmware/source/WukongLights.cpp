e1bdbd53d7928ec69e456778a9ffd1325340bea3
Endoxidev <113648540+MetaMysteries8@users.noreply.github.com>
2026-08-24 21:33:36 -0600

    Add Wukong base LED self-test and I2C status

--- firmware/source/WukongLights.cpp
+++ firmware/source/WukongLights.cpp
@@ -2,28 +2,53 @@
 #include "CodalFiber.h"
 
 using namespace codal;
+
+// ELECFREAKS uses 7-bit address 0x10. CODAL's I2C API takes the 8-bit bus
+// address, therefore the write address is 0x20.
 static const uint16_t WUKONG_I2C_ADDR = 0x20;
 
-void WukongLights::write4(uint8_t reg, uint8_t value) {
+WukongLights::WukongLights(I2C &bus) : i2c(bus), lastOk(true) {
+    i2c.setFrequency(100000);
+}
+
+bool WukongLights::write4(uint8_t reg, uint8_t value) {
     uint8_t b[4] = {reg, value, 0, 0};
-    i2c.write(WUKONG_I2C_ADDR, b, 4);
+    lastOk = (i2c.write(WUKONG_I2C_ADDR, b, 4) == DEVICE_OK);
+    return lastOk;
 }
 
-void WukongLights::breath() {
-    write4(0x11, 0);
+bool WukongLights::breath() {
+    bool a = write4(0x11, 0);
     fiber_sleep(100);
-    write4(0x12, 150);
+    bool b = write4(0x12, 150);
+    lastOk = a && b;
+    return lastOk;
 }
 
-void WukongLights::steady(uint8_t brightness) {
+bool WukongLights::steady(uint8_t brightness) {
     if (brightness > 100) brightness = 100;
-    write4(0x12, brightness);
+    bool a = write4(0x12, brightness);
     fiber_sleep(100);
-    write4(0x11, 160);
+    bool b = write4(0x11, 160);
+    lastOk = a && b;
+    return lastOk;
 }
 
-void WukongLights::off() {
-    write4(0x12, 0);
+bool WukongLights::off() {
+    bool a = write4(0x12, 0);
     fiber_sleep(100);
-    write4(0x11, 160);
+    bool b = write4(0x11, 160);
+    lastOk = a && b;
+    return lastOk;
+}
+
+bool WukongLights::selfTest() {
+    bool ok1 = off();
+    bool ok2 = steady(100);
+    fiber_sleep(140);
+    bool ok3 = off();
+    fiber_sleep(80);
+    bool ok4 = breath();
+    lastOk = ok1 && ok2 && ok3 && ok4;
+    return lastOk;
 }
